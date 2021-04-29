4d05da84743aa2c644c8a1e881c30dbebfa361f7
t-awka <80962656+t-awka@users.noreply.github.com>
2021-04-29 15:13:57 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+eleves.forEach (e => {
+    console.log("salut " +e);
+});
+
+// 2
+let wsh = ["Alexis", "Andy", "Charles", "Dorian", "Farhad"];
+wsh.forEach (e=> {
+    console.log(e);
+})
